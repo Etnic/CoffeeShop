69e6e54e39546b3098d89679d1e229576e75b99e
Etnic <kubus.martin@gmail.com>
2019-09-13 15:07:12 +0200

    2

--- CoffeeShop/CoffeeShop/Program.cs
+++ CoffeeShop/CoffeeShop/Program.cs
@@ -6,7 +6,7 @@ namespace CoffeeShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("dd!");
         }
     }
 }
