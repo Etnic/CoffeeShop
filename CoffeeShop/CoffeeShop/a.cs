2ef77a0dd0eba31a8b8bdca8bfd04cbb1024698f
Etnic <kubus.martin@gmail.com>
2019-09-14 10:57:54 +0200

    aaaa

--- CoffeeShop/CoffeeShop/a.cs
+++ CoffeeShop/CoffeeShop/a.cs
@@ -9,5 +9,7 @@ namespace CoffeeShop
     public class a
     {
         public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
     }
 }
